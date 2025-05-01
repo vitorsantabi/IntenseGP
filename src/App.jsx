7c9d7effcf672b1bd63c09270629fc16e7c54013
Jose Vitor <98997905+vitorsantabi@users.noreply.github.com>
2025-05-01 09:57:19 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Ola mundo!</h1>
+      <i class="fa-solid fa-face-laugh-wink"></i>
+    </>
+  )
+}
+
+export default App
